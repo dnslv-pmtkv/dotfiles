20024afab718b9233b7e6f0a530b46c743edaf7b
dnslv-pmtkv <254772902+dnslv-pmtkv@users.noreply.github.com>
2026-04-03 10:08:50 +0300

    Add sound config

---
+++ scripts/setup_audio.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+set -e
+
+SRC="$HOME/dotfiles/audio/virtual-sinks.conf"
+DEST="$HOME/.config/pipewire/pipewire-pulse.conf.d"
+
+link() {
+  local src="$1"
+  local dest="$2"
+
+  # If destination is a symlink pointing to the right place, do nothing
+  if [ -L "$dest" ] && [ "$(readlink "$dest")" = "$src" ]; then
+    echo "✔ $dest already linked"
+    return
+  fi
+
+  # If destination exists (file/dir or wrong symlink), back it up
+  if [ -e "$dest" ] || [ -L "$dest" ]; then
+    echo "Backing up $dest → $BACKUP_DIR"
+    mv "$dest" "$BACKUP_DIR/$(basename "$dest").bak"
+  fi
+
+  echo -e "Linking $src → $dest\n"
+  ln -s "$src" "$dest"
+}
+
+mkdir -p "$DEST"
+
+link "$SRC/virtual-sinks.conf" "$HOME/virtual-sinks.conf"
+
+systemctl --user restart pipewire pipewire-pulse wireplumber
+
+echo "Symlinks installed."
+
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -15,7 +15,7 @@ export ZSH="$HOME/.oh-my-zsh"
 # load a random theme each time Oh My Zsh is loaded, in which case,
 # to know which specific one was loaded, run: echo $RANDOM_THEME
 # See https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
-# ZSH_THEME="powerlevel10k/powerlevel10k"
+ZSH_THEME="powerlevel10k/powerlevel10k"
 
 # Set list of themes to pick from when loading at random
 # Setting this variable when ZSH_THEME=random will cause zsh to load
@@ -116,8 +116,6 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 . "$HOME/.local/bin/env"
-
-source ~/.powerlevel10k/powerlevel10k.zsh-theme
 
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
